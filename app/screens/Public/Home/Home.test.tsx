13ff93655f9790ab0516842c87e09d015aebb677
Marco Luz <marcotdl@hotmail.com>
2024-02-02 02:02:59 +0000

    Removed sample test. Removed common folder

--- app/screens/Public/Home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import Home from "./Home";
-
-describe("Home", () => {
-  it("renders the welcome message and login button", () => {
-    //render(<Home navigation={{ navigate: jest.fn() }} />);
-  });
-});
