4548796d3248d8e4c31826a1b832abc592acc29a
Marco Luz <marcotdl@hotmail.com>
2024-02-03 19:42:01 +0000

    Update on DriverDashboard

--- app/screens/Public/DriverDashboard/DriverDashboard.tsx
+++ app/screens/Public/DriverDashboard/DriverDashboard.tsx
@@ -1,8 +1,60 @@
 import BackButton from "@/app/components/BackButton/BackButton";
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const DriverDashboard = ({ navigation }: any) => {
+  const listData = [
+    {
+      id: "1",
+      distance: "1.3 miles",
+      items: "3 items",
+      time: "11:30AM-03:40PM",
+      address: "255 Disson Road, London, UK",
+      price: "25£",
+    },
+    {
+      id: "2",
+      distance: "5.6 miles",
+      items: "25 items",
+      time: "12:30AM-04:20PM",
+      address: "3 Cringe Drive, London, UK",
+      price: "35£",
+    },
+    {
+      id: "3",
+      distance: "1.3 miles",
+      items: "3 items",
+      time: "11:30AM-03:40PM",
+      address: "255 Disson Road, London, UK",
+      price: "25£",
+    },
+    {
+      id: "4",
+      distance: "1.3 miles",
+      items: "3 items",
+      time: "11:30AM-03:40PM",
+      address: "255 Disson Road, London, UK",
+      price: "25£",
+    },
+    {
+      id: "5",
+      distance: "1.3 miles",
+      items: "3 items",
+      time: "11:30AM-03:40PM",
+      address: "255 Disson Road, London, UK",
+      price: "25£",
+    },
+    {
+      id: "6",
+      distance: "1.3 miles",
+      items: "3 items",
+      time: "11:30AM-03:40PM",
+      address: "255 Disson Road, London, UK",
+      price: "25£",
+    },
+  ];
+
   return (
     <View className="bg-white h-full px-5 pt-14">
       <View className="flex-row items-center">
@@ -11,14 +63,47 @@ const DriverDashboard = ({ navigation }: any) => {
           Driver Dashboard
         </Text>
       </View>
-      <TouchableOpacity className="bg-white w-full h-48 rounded-2xl p-5 shadow-sm mt-5">
-        <View className="flex-row justify-between">
-          <Text className="font-bold">1.3 miles</Text>
-          <Text className="font-bold">3 items</Text>
-          <Text className="font-bold">11:30AM-03:40PM</Text>
-        </View>
-        <Text>255 Disson Road, London, UK</Text>
-        <Text className="">25£</Text>
+      <FlatList
+        data={listData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            className={`bg-white w-full h-48 rounded-2xl p-5 shadow-sm mt-5 flex justify-between ${
+              index === listData.length - 1 ? "mb-32" : ""
+            }`}
+          >
+            <View className="flex-row justify-between">
+              <Text className="font-bold text-emerald-500 text-base">
+                {item.distance}
+              </Text>
+
+              <Text className="font-bold text-emerald-500 text-base">
+                {item.time}
+              </Text>
+            </View>
+            <View className="flex justify-between flex-1">
+              <Text className="font-bold text-emerald-500 text-base">
+                {item.items}
+              </Text>
+              <Text className="text-base text-gray-500">{item.address}</Text>
+              <View className="flex-row items-end justify-between">
+                <TouchableOpacity className="bg-red-600 items-center rounded-full">
+                  <FontAwesome5 name="times-circle" size={30} color="white" />
+                </TouchableOpacity>
+                <Text className="font-bold text-emerald-600 text-base">
+                  {item.price}
+                </Text>
+
+                <TouchableOpacity className="bg-green-400 items-center rounded-full">
+                  <FontAwesome5 name="check-circle" size={30} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+      <TouchableOpacity className="m-5 py-4 bg-emerald-700 rounded-3xl items-center absolute w-full bottom-5 z-10">
+        <Text className="text-white font-semibold text-2xl">Go Online</Text>
       </TouchableOpacity>
     </View>
   );
