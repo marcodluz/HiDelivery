fb567b7c31101ac37617a0e2c9f7c4eb9a4b0407
Marco Luz <marcotdl@hotmail.com>
2024-02-18 21:48:16 +0000

    Coverage jest command and configs

--- jest.config.js
+++ jest.config.js
@@ -6,3 +6,11 @@ module.exports = {
   testMatch: ["**/?(*.)+(spec|test).(ts|tsx)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
+
+module.exports = {
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}", // Adjust the path according to your source files
+    "!src/**/*.d.ts",
+    // Exclude other files/folders as needed
+  ],
+};
