1555c69e8850b978a866b233c662368cfe445571
Marco Luz <marcotdl@hotmail.com>
2024-02-24 16:22:23 +0000

    UI of order page

--- app/screens/Public/DriverDashboard/ViewOrder.tsx
+++ app/screens/Public/DriverDashboard/ViewOrder.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { useOrder } from "@/app/context/OrderContext";
 
@@ -9,8 +9,8 @@ const ViewOrder = () => {
   return (
     <View className="bg-white h-full">
       <View className="p-5">
-        <View className="bg-red-100 w-full flex justify-between">
-          <Text className="font-bold text-xl">Order #001</Text>
+        <View className="w-full flex justify-between">
+          <Text className="font-bold text-3xl">Order #0001</Text>
           <View className="flex-row gap-3 h-32">
             <View className="justify-around">
               <FontAwesome5 name="map-marker-alt" size={18} color="black" />
@@ -27,6 +27,19 @@ const ViewOrder = () => {
             </View>
           </View>
         </View>
+        <ScrollView className="h-full">
+          <TouchableOpacity className="flex-row gap-x-5">
+            <Image
+              source={require("@/assets/burger.jpg")}
+              className="rounded-2xl w-1/3 aspect-square"
+            />
+            <View className="flex-col w-3/5 justify-between">
+              <Text className="font-medium text-lg">Original Cheeseburger</Text>
+              <Text className="font-bold text-xl">£10.99 - £15.99</Text>
+              <Text className="font-bold text-xl text-emerald-500">Done</Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </View>
   );
