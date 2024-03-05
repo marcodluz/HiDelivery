4778fd39faebb23b726eb81b6cfff368ca69bffb
Marco Luz <marcotdl@hotmail.com>
2024-03-05 22:49:04 +0000

    Added react native firebase packages

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "@/app/screens/Public/Home/Home";
 import Login from "@/app/screens/Auth/Login/Login";
