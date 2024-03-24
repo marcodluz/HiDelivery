640b5c0cfc5565a34702f2123ceee2611fb7270e
Marco Luz <marcotdl@hotmail.com>
2024-03-24 20:57:28 +0000

    Added the buttons and input field to the Login screen

--- app/screens/Auth/Login/Login.tsx
+++ app/screens/Auth/Login/Login.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import TextField from "@mui/material/TextField";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const Login = () => {
   const [email, setEmail] = React.useState("");
@@ -15,8 +15,57 @@ const Login = () => {
           </Text>
         </View>
       </View>
-      <View className="w-full">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <View className="w-full mt-14 mb-5">
+        <TextInput
+          className="border border-slate-200 px-4 py-5 rounded-xl"
+          placeholder="Email Address"
+          keyboardType="email-address"
+          inputMode="email"
+          textContentType="emailAddress"
+          autoComplete="email"
+          clearButtonMode="while-editing"
+          returnKeyType="next"
+          autoCapitalize="none"
+          maxLength={254}
+        />
+        <TouchableOpacity
+          onPress={() => {}}
+          className="h-14 mt-5 bg-sky-950 rounded-xl items-center w-full overflow-hidden justify-center"
+        >
+          <Text className="text-white font-normal text-lg">Continue</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="w-full gap-y-3">
+        <TouchableOpacity
+          onPress={() => {}}
+          className="h-14 bg-slate-300 rounded-xl items-center w-full overflow-hidden justify-center"
+        >
+          <Text className="text-black font-normal text-lg">
+            <FontAwesome6 name="apple" size={20} color="black" />
+            {"  "}
+            Continue with Apple
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          className="h-14 bg-slate-300 rounded-xl items-center w-full overflow-hidden justify-center"
+        >
+          <Text className="text-black font-normal text-lg">
+            <FontAwesome6 name="google" size={18} color="black" />
+            {"  "}
+            Continue with Google
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          className="h-14 bg-slate-300 rounded-xl items-center w-full overflow-hidden justify-center"
+        >
+          <Text className="text-black font-normal text-lg">
+            <FontAwesome6 name="facebook" size={18} color="black" />
+            {"  "}
+            Continue with Facebook
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
