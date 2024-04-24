24f8d5935c1b6a3526436e03201ed85daed34a25
Marco Luz <marco.luz@theaccessgroup.com>
2024-04-24 11:34:11 +0100

    Designed Account page

--- app/screens/Customer/Account/Account.tsx
+++ app/screens/Customer/Account/Account.tsx
@@ -1,9 +1,10 @@
 import { useAuth } from "@/app/context/AuthContext";
 import { IUser } from "@/app/interfaces/IUser";
 import React, { useState } from "react";
-import { Alert, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { defaultScreen } from "@/app/styles/Global";
 import { useNavigation } from "@react-navigation/native";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const Account = () => {
   const { userSignOut, getUserData } = useAuth();
@@ -30,12 +31,29 @@ const Account = () => {
         </Text>
       </View>
       <View className="justify-between flex-1">
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Privacy")}
-          className="h-14 mt-5 bg-gray-100 rounded-full w-full overflow-hidden justify-center items-center"
-        >
-          <Text className="text-black text-xl">Privacy</Text>
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity className="h-14 mt-5 overflow-hidden items-center flex-row">
+            <FontAwesome6 name="user" size={22} color="black" />
+            <Text className="text-black text-xl ml-2">Manage Account</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Privacy")}
+            className="h-14 mt-5 overflow-hidden items-center flex-row"
+          >
+            <FontAwesome6 name="eye" size={22} color="black" />
+            <Text className="text-black text-xl ml-2">Privacy</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Driver Dashboard")}
+            className="h-14 mt-5 overflow-hidden items-center flex-row"
+          >
+            <FontAwesome6 name="car" size={22} color="black" />
+            <Text className="text-black text-xl ml-2">Driver Dashboard</Text>
+            <View className="bg-green-300 py-1 px-4 rounded-full ml-2">
+              <Text className="font-bold">Apply now</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
         <Text
           onPress={handleSignOut}
           className="text-rose-600 font-semibold text-base text-center"
