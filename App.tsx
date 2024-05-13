3d49b03c3c0a32237a0a12c279c2452a5b11627b
Marco Luz <marcotdl@hotmail.com>
2024-05-13 22:30:05 +0100

    Fixed the crashing React Navigation

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { AddressProvider } from "@/app/context/AddressContext";
 import { OrderProvider } from "@/app/context/OrderContext";
 import { AuthProvider } from "@/app/context/AuthContext";
 
+import "react-native-reanimated"; // Fix for crashing SDK 51
+
 import Navigation from "@/app/Navigation";
 
 const App = () => {
