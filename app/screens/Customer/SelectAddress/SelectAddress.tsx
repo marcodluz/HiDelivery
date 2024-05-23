00125669cc9fb852126f553020207fed9003e1e4
Marco Luz <marcotdl@hotmail.com>
2024-05-23 22:20:32 +0100

    Removed backbutton

--- app/screens/Customer/SelectAddress/SelectAddress.tsx
+++ app/screens/Customer/SelectAddress/SelectAddress.tsx
@@ -3,16 +3,13 @@ import AddressSearchBar from "@/app/components/addressSearchBar/AddressSearchBar
 import { View } from "react-native";
 import { useAddress } from "@/app/context/AddressContext";
 import Map from "@/app/components/map/Map";
-import BackButton from "@/app/components/backButton/BackButton";
-import NearbyShops from "@/app/components/nearbyShops/NearbyShops";
 
-const Products = ({ navigation }: any) => {
+const Products = () => {
   console.log("Selected Address: " + useAddress().address);
 
   return (
     <View className="bg-white h-full">
       <View className="px-5 z-10 pt-14">
-        <BackButton navigation={navigation} />
         <View className="mt-4 z-10 flex-1">
           <AddressSearchBar />
         </View>
